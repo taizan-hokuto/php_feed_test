a8182d7ecfc240b1378ad407e0a582250cbed3e2
taizan-hokuto <55448286+taizan-hokuto@users.noreply.github.com>
2019-12-04 22:59:46 +0900

    Fix comments

--- index2.php
+++ index2.php
@@ -52,7 +52,7 @@
 
   function GetOtherStatus($video_id, $api_key){
     //複数のフラグを取得するサンプル"
-    //1回あたりのapiコスト：約
+    //1回あたりのapiコスト：約5
     $url = "https://www.googleapis.com/youtube/v3/videos?part=snippet,liveStreamingDetails&fields=items(id,snippet(title,liveBroadcastContent),liveStreamingDetails(actualStartTime,actualEndTime,concurrentViewers))&id=".$video_id."&key=".$api_key;
 
     $ch = curl_init();
